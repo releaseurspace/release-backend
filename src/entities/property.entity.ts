20734757088a0f23bcd7ffc7d8099c3dd985e454
JeongYeonSeung <asas4654@daum.net>
2025-01-09 14:33:28 +0900

    feat: common entity, property entity 추가

---
+++ src/entities/property.entity.ts
@@ -0,0 +1,77 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CommonEntity } from './common.entity';
+
+@Entity('property')
+export class PropertyEntity extends CommonEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  address: string;
+
+  @Column('varchar')
+  latitude: string;
+
+  @Column('varchar')
+  longitude: string;
+
+  @Column('varchar')
+  purpose: string;
+
+  @Column('varchar')
+  floor: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  size_sqm: number;
+
+  @Column('text')
+  description: string;
+
+  @Column('text')
+  building_info: string;
+
+  @Column('varchar', { nullable: true })
+  nearest_station: string;
+
+  @Column('varchar', { nullable: true })
+  distance_to_station: string;
+
+  @Column('varchar')
+  monthly_rent: string;
+
+  @Column('varchar')
+  deposit: string;
+
+  @Column('varchar')
+  key_money: string;
+
+  @Column('varchar')
+  maintenance_fee: string;
+
+  @Column('text')
+  agent_comment: string;
+
+  @Column('varchar')
+  rental_status: string;
+
+  @Column('varchar')
+  available_date: string;
+
+  @Column('varchar')
+  parking_info: string;
+
+  @Column('varchar')
+  bathroom_info: string;
+
+  @Column('int')
+  elavator_count: number;
+
+  @Column('varchar')
+  heating_type: string;
+
+  @Column('varchar')
+  direction: string;
+
+  @Column('varchar')
+  property_registry: string;
+}
