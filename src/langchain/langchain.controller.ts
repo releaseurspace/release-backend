f77ac985e349244cc3a8ac1720e8561b87f00bfd
JeongYeonSeung <asas4654@daum.net>
2025-01-19 20:47:05 +0900

    mod: 응답 dto에 맞게 chat 함수 로직 및  반환값 변경

--- src/langchain/langchain.controller.ts
+++ src/langchain/langchain.controller.ts
@@ -1,14 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LangchainService } from './langchain.service';
-import { AIMessageChunk } from '@langchain/core/messages';
 import { ChatInputDto } from './dtos/chat-input.dto';
+import { ChatResponseDto } from './dtos/char-response.dto';
 
 @Controller('langchain')
 export class LangchainController {
   constructor(private readonly langchainService: LangchainService) {}
 
   @Post()
-  async test(@Body() body: ChatInputDto): Promise<AIMessageChunk> {
-    return await this.langchainService.test(body);
+  async chat(@Body() body: ChatInputDto): Promise<ChatResponseDto> {
+    return await this.langchainService.chat(body);
   }
 }
--- src/langchain/langchain.service.ts
+++ src/langchain/langchain.service.ts
@@ -1,16 +1,23 @@
-import { AIMessageChunk } from '@langchain/core/messages';
 import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { createLangGraphWorkflow } from 'src/common/langgraph.workflow';
 import { ChatConfig } from 'src/common/types/chat-config.interface';
 import { ChatInputDto } from './dtos/chat-input.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PropertyEntity } from 'src/entities/property.entity';
+import { ChatResponseDto } from './dtos/char-response.dto';
+import { GetPropertyDto } from './dtos/get-property.dto';
 
 @Injectable()
 export class LangchainService {
   private readonly langGraphApp;
   private readonly userSessions: Map<string, string>;
 
-  constructor() {
+  constructor(
+    @InjectRepository(PropertyEntity)
+    private readonly propertyRepository: Repository<PropertyEntity>,
+  ) {
     this.langGraphApp = createLangGraphWorkflow();
     this.userSessions = new Map();
   }
@@ -24,7 +31,7 @@ export class LangchainService {
     return threadId;
   }
 
-  async test(body: ChatInputDto): Promise<AIMessageChunk> {
+  async chat(body: ChatInputDto): Promise<ChatResponseDto> {
     const { userId, content } = body;
     const threadId = this.getOrCreateThreadId(userId);
     const config: ChatConfig = {
@@ -38,6 +45,27 @@ export class LangchainService {
         content,
       },
     };
-    return await this.langGraphApp.invoke(input, config);
+    const llmReponse = await this.langGraphApp.invoke(input, config);
+    if (llmReponse.vectors && llmReponse.route === 'SEARCH') {
+      const propertyIds = llmReponse.vectors.map((vector) => {
+        return vector.metadata.psql_id;
+      });
+      const properties = await this.getProperties(propertyIds);
+      return new ChatResponseDto(llmReponse.currentResponse, properties);
+    } else {
+      return new ChatResponseDto(llmReponse.currentResponse, []);
+    }
+  }
+
+  async getProperties(ids: number[]): Promise<GetPropertyDto[]> {
+    if (ids.length === 0) return [];
+    return await Promise.all(
+      ids.map(async (id) => {
+        const property = await this.propertyRepository.findOne({
+          where: { id },
+        });
+        return new GetPropertyDto(property);
+      }),
+    );
   }
 }
